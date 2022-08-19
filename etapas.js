66572866584a861d7f8abc29bb4475fc69d4d8db
Malicscki <106198134+Malicscki@users.noreply.github.com>
2022-08-19 19:35:22 -0300

    etapa de inicio js

--- etapas.js
+++ etapas.js
@@ -1,50 +1,51 @@
 let etapas = [
     {
-    titulo: 'VEREADOR',
-    numeros: 5,
-    candidatos: [
-        {
-            numero: 38111,
-            name: 'Fulano de Tal',
-            partido: 'ABC',
-            fotos: [
-                {url: '38111.jpg', legenda: 'Vereador', small: false}
-            ]
-        },
-        {
-            numero: 77222,
-            name: 'Beltrano da Silva',
-            partido: 'DEFG',
-            fotos: [
-                {url:'77222 .jpg', legenda: 'Vereador'}
-            ]
-        },
-    ]
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: 'imagens/nazare.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
     },
     {
         titulo: 'PREFEITO',
         numeros: 2,
         candidatos: [
         {
-        numero: 99,
-        name: 'Ciclano',
-        partido: 'ABC',
-        vice: 'Cic',
-        fotos: [
-        {url: '99.jpg', legenda: 'Prefeito'},
-        {url: '99 2.jpg', legenda: 'Vice-Prefeito', small:true}
-        ]
-   },
-   {
-    numero:84,
-    name: 'Zulano',
-    partido: 'QWERTY',
-    Vice: 'Zul',
-    fotos:[
-        {url: '99.jpg', legenda: 'Prefeito'},
-        {url: '99 2.jpg', legenda: 'Vice-Prefeito', small:true}
-        ]
+            numero: 99,
+            nome: 'Ciclano',
+            partido: 'ABC',
+            vice: 'Cic',
+            fotos: [
+            {url: '99.jpg', legenda: 'Prefeito'},
+            {url: '992.jpg', legenda: 'Vice-Prefeito', small:true}
+            ]
     },
-]
+    {
+            numero:84,
+            nome: 'Zulano',
+            partido: 'QWERTY',
+            Vice: 'Zul',
+            fotos:[
+                {url: '99.jpg', legenda: 'Prefeito'},
+                {url: '992.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+        },
+    ]
 }
 ];
+
