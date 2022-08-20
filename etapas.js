78fc9b5198e5eed0dbac5c28ada255c46c1d6427
Malicscki <106198134+Malicscki@users.noreply.github.com>
2022-08-20 11:17:52 -0300

    etapas finalizado

--- etapas.js
+++ etapas.js
@@ -4,19 +4,19 @@ let etapas = [
         numeros: 5,
         candidatos: [
             {
-                numero: 38111,
-                nome: 'Fulano de Tal',
-                partido: 'ABC',
+                numero: '38111',
+                nome: 'Nazaré',
+                partido: 'GLOBAL',
                 fotos: [
-                    {url: 'imagens/nazare.jpg', legenda: 'Vereador', small: false}
+                    {url: 'nazare.jpg', legenda: 'Vereador', small: false}
                 ]
             },
             {
-                numero: 77222,
-                nome: 'Beltrano da Silva',
-                partido: 'DEFG',
+                numero: '77222',
+                nome: 'Glória Pires',
+                partido: 'GLOBAL',
                 fotos: [
-                    {url:'77222.jpg', legenda: 'Vereador'}
+                    {url:'gloria-pires.jpg', legenda: 'Vereador'}
                 ]
             },
         ]
@@ -26,23 +26,23 @@ let etapas = [
         numeros: 2,
         candidatos: [
         {
-            numero: 99,
-            nome: 'Ciclano',
+            numero: '99',
+            nome: 'Pele de cobra',
             partido: 'ABC',
-            vice: 'Cic',
+            vice: 'Jeremias Maluco',
             fotos: [
-            {url: '99.jpg', legenda: 'Prefeito'},
-            {url: '992.jpg', legenda: 'Vice-Prefeito', small:true}
+            {url: 'pele-de-cobra.jpg', legenda: 'Prefeito'},
+            {url: 'jeremias-maluco.jpg', legenda: 'Vice-Prefeito', small:true}
             ]
     },
     {
-            numero:84,
+            numero: '84',
             nome: 'Zulano',
             partido: 'QWERTY',
             Vice: 'Zul',
             fotos:[
-                {url: '99.jpg', legenda: 'Prefeito'},
-                {url: '992.jpg', legenda: 'Vice-Prefeito', small:true}
+                {url: 'serjao-berranteiro.jpg', legenda: 'Prefeito'},
+                {url: 'rei-do-camarote.jpg', legenda: 'Vice-Prefeito', small:true}
                 ]
         },
     ]
